c7484cff02e607dec1bb70e904682ed7ea884bb0
Jose Ortiz <wayko@users.noreply.github.com>
2024-12-04 19:39:31 -0500

    Update TroubleTicket.php
    Removed Button

--- PHPFiles/TroubleTicket.php
+++ PHPFiles/TroubleTicket.php
@@ -10,12 +10,6 @@
             <nav class="navbar navbar-default">
                 <div class="container-fluid">
                     <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#cartNav" aria-expanded="false">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                             <span class="icon-bar"></span>
-                              <span class="icon-bar"></span>
-                        </button> 
                         <a class="navbar-brand" href="#">HOME</a>
                         <a class="navbar-brand" href="troubleticket.php">Ticket System</a>
                     </div>
